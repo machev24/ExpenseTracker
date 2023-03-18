b2379cc085d3cd1cd2d753ea5c9a352a9f23602a
machev24 <machevivan3@gmail.com>
2023-03-18 14:18:45 +0200

    18.03.2023

--- Data/Models/Category.cs
+++ Data/Models/Category.cs
@@ -1,16 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+/// <summary>
+/// Data Models Category namespace
+/// </summary>
 namespace Data.Models
 {
+    /// <summary>
+    /// Data Models Category Class
+    /// </summary>
     public class Category
     {
+        /// <summary>
+        /// Category ID
+        /// </summary>
         [Key]
         public int CategoryId { get; set; }
 
+        /// <summary>
+        /// Category Title
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// Category Icon
+        /// </summary>
         public string Icon { get; set; }
 
+        /// <summary>
+        /// Category Type
+        /// </summary>
         public string Type { get; set; }
     }
 }
--- Data/Models/Transcaction.cs
+++ Data/Models/Transcaction.cs
@@ -1,19 +1,44 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+/// <summary>
+/// Data Models Transaction namespace
+/// </summary>
 namespace Data.Models
 {
+    /// <summary>
+    /// Data Models Transaction Class
+    /// </summary>
     public class Transcaction
     {
+        /// <summary>
+        /// Transaction ID
+        /// </summary>
         [Key]
         public int TransactionId { get; set; }
 
+        /// <summary>
+        /// Category ID
+        /// </summary>
         public int CategoryId { get; set; }
+
+        /// <summary>
+        /// Category from Data.Models.Category
+        /// </summary>
         public Category Category { get; set; }
 
+        /// <summary>
+        /// Transaction amount
+        /// </summary>
         public decimal Amount { get; set; }
 
+        /// <summary>
+        /// Transaction note
+        /// </summary>
         public string Note { get; set; }
 
+        /// <summary>
+        /// Transaction date
+        /// </summary>
         public DateTime Date { get; set; } = DateTime.Now;
     }
 }
