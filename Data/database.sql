85c5d211a48392e42225aa27b82c9332592b9876
machev24 <machevivan3@gmail.com>
2023-03-18 14:00:44 +0200

    18.03.2023

---
+++ Data/database.sql
@@ -0,0 +1,19 @@
+﻿USE [ExpenseTracker];
+
+-- Create table
+CREATE TABLE [Category] 
+(
+	[id] INT, 
+	[title] VARCHAR(50), 
+	[icon] VARCHAR(50),
+	[type] VARCHAR(50)
+);
+
+CREATE TABLE [Transaction] 
+(
+	[id] INT, 
+	[category] INT, 
+	[amount] DECIMAL,
+	[note]VARCHAR(75),
+	[date] DATETIME
+);
