5a9e29130ac460f96fd49f00e91c1e7cce559f66
machev24 <machevivan3@gmail.com>
2023-03-18 14:12:37 +0200

    18.03.2023

--- Data/Models/Category.cs
+++ Data/Models/Category.cs
@@ -1,6 +1,16 @@
-﻿namespace Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models
 {
     public class Category
     {
+        [Key]
+        public int CategoryId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Type { get; set; }
     }
 }
--- Data/Models/Transcaction.cs
+++ Data/Models/Transcaction.cs
@@ -1,6 +1,19 @@
-﻿namespace Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models
 {
     public class Transcaction
     {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
     }
 }
