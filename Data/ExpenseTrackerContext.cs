cfd9298606d001602f8bccfe6f0a6d4f1487e1c4
machev24 <machevivan3@gmail.com>
2023-03-18 14:24:41 +0200

    18.03.2023

---
+++ Data/ExpenseTrackerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+/// <summary>
+/// Expense Tracker Data namespace
+/// </summary>
+namespace Data
+{
+    /// <summary>
+    /// Db Context class
+    /// </summary>
+    public class ExpenseTrackerContext: DbContext
+    {
+
+    }
+}
