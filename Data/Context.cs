d67b14badd1ab9fd1161fd573a627bce886787b4
machev24 <machevivan3@gmail.com>
2023-03-18 15:07:56 +0200

    18.03.2023

--- Data/Context.cs
+++ Data/Context.cs
@@ -12,16 +12,32 @@ namespace Data
     /// </summary>
     public class Context: DbContext
     {
+        /// <summary>
+        /// Database Connection String
+        /// </summary>
         private const string connectionString = "";
 
+        /// <summary>
+        /// Transaction Table from Database
+        /// </summary>
         public DbSet<Transaction> Transactions { get; set; }
+
+        /// <summary>
+        /// Category Table from Database
+        /// </summary>
         public DbSet<Category> Categories { get; set; }
 
+        /// <summary>
+        /// Default Construstor
+        /// </summary>
         public Context(): base()
         {
-
+            //empty
         }
 
+        /// <summary>
+        /// Set Connection String Configuration
+        /// </summary>
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString);
