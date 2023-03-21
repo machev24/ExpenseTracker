3efe2a130c5b2420db76147256610ff9d2be09ef
steliyan17 <stelianatanasov777@gmail.com>
2023-03-21 17:35:55 +0200

    NUnit Tests Project

---
+++ ExpenseTrackerNUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ExpenseTrackerNUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
