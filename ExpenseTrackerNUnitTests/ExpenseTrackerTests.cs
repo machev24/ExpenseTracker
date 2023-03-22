8a7df059e1630c108c14a4cadffe55e7fd270b45
steliyan17 <stelianatanasov777@gmail.com>
2023-03-22 18:56:35 +0200

    one test readyy

--- ExpenseTrackerNUnitTests/ExpenseTrackerTests.cs
+++ ExpenseTrackerNUnitTests/ExpenseTrackerTests.cs
@@ -1,134 +1,34 @@
-using NUnit.Framework;
+using Data;
 using Data.Models;
-using Business;
+using Moq;
+using NUnit.Framework;
 using System;
-using System.Linq;
-using System.Collections.Generic;
 
-namespace ExpenseTrackerNUnitTests
+namespace Business.Tests
 {
-    public class TransactionBusinessTests
+    [TestFixture]
+    public class CategoryBusinessTests
     {
-        private TransactionBusiness transactionBusiness;
-
+        private CategoryBusiness categoryBusiness;
 
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
-            transactionBusiness = new TransactionBusiness();
+            categoryBusiness = new CategoryBusiness();
         }
 
-        [Test]
-        public void GetAll_ReturnsAllTransactions()
-        {
-            //Arrange
-            var expectedCount = 3;
-
-            //Act
-            var result = transactionBusiness.GetAll();
-
-            //Assert
-            Assert.IsNotNull(result);
-
-            Assert.AreEqual(expectedCount, result.Count);
-        }
 
         [Test]
-        public void Get_ReturnsTransaction()
+        public void Get_WithInvalidId_ReturnsNull()
         {
-            //Arrange
-            var expectedTransaction = new Transaction()
-            {
-                id = 1,
-                date = new DateTime(2021, 06, 01),
-                amount = 50,
-                category = 1,
-                note = "Transaction 1"
-            };
+            var category = categoryBusiness.Get(-1);
 
-            //Act
-            var result = transactionBusiness.Get(1);
-
-            //Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(expectedTransaction.id, result.id);
-            Assert.AreEqual(expectedTransaction.date, result.date);
-            Assert.AreEqual(expectedTransaction.amount, result.amount);
-            Assert.AreEqual(expectedTransaction.category, result.category);
-            Assert.AreEqual(expectedTransaction.note, result.note);
-        }
-
-        [Test]
-        public void Add_AddsTransaction()
-        {
-            //Arrange
-            var transaction = new Transaction()
-            {
-                date = new DateTime(2021, 06, 02),
-                amount = 100,
-                category = 2,
-                note = "Transaction 4"
-            };
-
-            //Act
-            transactionBusiness.Add(transaction);
-
-            var result = transactionBusiness.Get(4);
-
-            //Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(transaction.date, result.date);
-            Assert.AreEqual(transaction.amount, result.amount);
-            Assert.AreEqual(transaction.category, result.category);
-            Assert.AreEqual(transaction.note, result.note);
-        }
-
-        [Test]
-        public void Update_UpdatesTransaction()
-        {
-            //Arrange
-            var transaction = new Transaction()
-            {
-                id = 2,
-                date = new DateTime(2021, 06, 03),
-                amount = 75,
-                category = 3,
-                note = "Updated Transaction 2"
-            };
-
-            //Act
-            transactionBusiness.Update(transaction);
-
-            var result = transactionBusiness.Get(2);
-
-            //Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(transaction.id, result.id);
-            Assert.AreEqual(transaction.date, result.date);
-            Assert.AreEqual(transaction.amount, result.amount);
-            Assert.AreEqual(transaction.category, result.category);
-            Assert.AreEqual(transaction.note, result.note);
-        }
-
-        [Test]
-        public void Delete_DeletesTransaction()
-        {
-            //Arrange
-            var transactionCountBeforeDelete = transactionBusiness.GetAll().Count;
-
-            //Act
-            transactionBusiness.Delete(2);
-
-            var result = transactionBusiness.GetAll();
-            var transactionCountAfterDelete = result.Count;
-            var deletedTransaction = result.FirstOrDefault(x => x.id == 2);
-
-            //Assert
-            Assert.IsNotNull(deletedTransaction);
-            Assert.That(transactionCountAfterDelete, Is.EqualTo(transactionCountBeforeDelete - 1));
+            Assert.IsNull(category);
         }
     }
 }
 
 
+      
+        
 
