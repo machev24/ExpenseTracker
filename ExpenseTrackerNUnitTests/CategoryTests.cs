5babdc22dcc6c7314da479463783f25d14c33846
steliyan17 <stelianatanasov777@gmail.com>
2023-03-25 18:21:47 +0200

    TestTransactionId

--- ExpenseTrackerNUnitTests/CategoryTests.cs
+++ ExpenseTrackerNUnitTests/CategoryTests.cs
@@ -2,7 +2,7 @@
 using NUnit.Framework;
 using System.ComponentModel.DataAnnotations;
 
-namespace Data.Tests.Models
+namespace Tests
 {
     public class CategoryTests
     {
---
+++ ExpenseTrackerNUnitTests/TransactionTests.cs
@@ -0,0 +1,16 @@
+﻿using Data.Models;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class TransactionTests
+    {
+        [Test]
+        public void TestTransactionId()
+        {
+            var transaction = new Transaction { Id = 1 };
+            Assert.AreEqual(1, transaction.Id);
+        }
+
+    }
+}
