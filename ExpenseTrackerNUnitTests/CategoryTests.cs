3672acc70b4e5afebdef1a8e7608c7faf68d2a7a
steliyan17 <stelianatanasov777@gmail.com>
2023-03-25 18:15:04 +0200

    Category_Id_Should_Have_KeyAttribute()

---
+++ ExpenseTrackerNUnitTests/CategoryTests.cs
@@ -0,0 +1,64 @@
+﻿using Data.Models;
+using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Tests.Models
+{
+    public class CategoryTests
+    {
+        [Test]
+        public void Category_Id_Should_Have_KeyAttribute()
+        {
+            // Arrange
+            var category = new Category();
+
+            // Act
+            var idProperty = category.GetType().GetProperty("Id");
+
+            // Assert
+            Assert.That(Attribute.IsDefined(idProperty, typeof(KeyAttribute)));
+        }
+
+        [Test]
+        public void Category_Title_Should_Be_Settable_And_Gettable()
+        {
+            // Arrange
+            var category = new Category();
+            var expectedTitle = "Test Category";
+
+            // Act
+            category.Title = expectedTitle;
+
+            // Assert
+            Assert.AreEqual(expectedTitle, category.Title);
+        }
+
+        [Test]
+        public void Category_Icon_Should_Be_Settable_And_Gettable()
+        {
+            // Arrange
+            var category = new Category();
+            var expectedIcon = "fa fa-car";
+
+            // Act
+            category.Icon = expectedIcon;
+
+            // Assert
+            Assert.AreEqual(expectedIcon, category.Icon);
+        }
+
+        [Test]
+        public void Category_Type_Should_Be_Settable_And_Gettable()
+        {
+            // Arrange
+            var category = new Category();
+            var expectedType = "expense";
+
+            // Act
+            category.Type = expectedType;
+
+            // Assert
+            Assert.AreEqual(expectedType, category.Type);
+        }
+    }
+}
