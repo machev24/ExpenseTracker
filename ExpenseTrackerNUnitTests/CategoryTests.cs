809cb08103b99919c2decb7546aea99faf0b77e2
steliyan17 <stelianatanasov777@gmail.com>
2023-03-25 18:16:45 +0200

    CategoryTitleShouldBeSettableAndGettable

--- ExpenseTrackerNUnitTests/CategoryTests.cs
+++ ExpenseTrackerNUnitTests/CategoryTests.cs
@@ -18,7 +18,6 @@ namespace Data.Tests.Models
             // Assert
             Assert.That(Attribute.IsDefined(idProperty, typeof(KeyAttribute)));
         }
-
         [Test]
         public void Category_Title_Should_Be_Settable_And_Gettable()
         {
@@ -32,33 +31,7 @@ namespace Data.Tests.Models
             // Assert
             Assert.AreEqual(expectedTitle, category.Title);
         }
-
-        [Test]
-        public void Category_Icon_Should_Be_Settable_And_Gettable()
-        {
-            // Arrange
-            var category = new Category();
-            var expectedIcon = "fa fa-car";
-
-            // Act
-            category.Icon = expectedIcon;
-
-            // Assert
-            Assert.AreEqual(expectedIcon, category.Icon);
-        }
-
-        [Test]
-        public void Category_Type_Should_Be_Settable_And_Gettable()
-        {
-            // Arrange
-            var category = new Category();
-            var expectedType = "expense";
-
-            // Act
-            category.Type = expectedType;
-
-            // Assert
-            Assert.AreEqual(expectedType, category.Type);
-        }
     }
 }
+
+       
